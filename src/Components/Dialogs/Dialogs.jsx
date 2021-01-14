a22e39d367cebf191e50a875e6cc4589f146c608
llgiant <llgiant@gmail.com>
2021-01-14 22:48:40 +0600

    Dialogs added

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-import s from  './Dialogs.Module.css';
+import s from './Dialogs.Module.css';
 
 
-const Dialogs = (props) =>{
-    return(
-        <div>
-            Dialogs
+const Dialogs = (props) => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dislogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Shaha
+                </div>
+                <div className={s.dialog}>
+                    Lena
+                </div>
+                <div className={s.dialog}>
+                    Rita
+                </div>
+                <div className={s.dialog}>
+                    Antoha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hello World!</div>
+                <div className={s.dialog}>Message from 2030!!</div>
+                <div className={s.dialog}>Karamba!</div>
+            </div>
         </div>
     )
 }
