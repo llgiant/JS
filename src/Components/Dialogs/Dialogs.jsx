a5357b40119ecd28dc8652ed87e8c2b4802c03e6
Bogdan <llgiant@gmail.com>
2021-01-18 17:50:51 +0600

    Added links to dialogs

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 import s from './Dialogs.Module.css';
+import {BrowserRouter, NavLink} from "react-router-dom";
 
 
 const Dialogs = (props) => {
     return (
+        <BrowserRouter>
         <div className={s.dialogs}>
             <div className={s.dislogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Victor
+                    <NavLink to="/Victor" activeClassName={s.activeLink}>Victor
+                    </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Shaha
+                    <NavLink to="/Shaha" activeClassName={s.activeLink}>Shaha </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Lena
+                    <NavLink to="/Lena" activeClassName={s.activeLink}> Lena </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Rita
+                    <NavLink to="/Rita" activeClassName={s.activeLink}>Rita </NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Antoha
+                    <NavLink to="/Antoha" activeClassName={s.activeLink}>Antoha </NavLink>
                 </div>
             </div>
+
             <div className={s.messages}>
                 <div className={s.dialog}>Hello World!</div>
                 <div className={s.dialog}>Message from 2030!!</div>
                 <div className={s.dialog}>Karamba!</div>
             </div>
         </div>
+        </BrowserRouter>
+
     )
 }
 export default Dialogs;
